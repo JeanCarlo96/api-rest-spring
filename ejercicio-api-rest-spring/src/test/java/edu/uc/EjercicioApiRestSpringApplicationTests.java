f6593b8048047c6f846dbbb827f250fc422de816
Jean Marchesini <jeancarlo.marchesini@yahoo.es>
2025-01-12 23:31:53 -0500

    JCMA Configuración Inicial del Proyecto

---
+++ ejercicio-api-rest-spring/src/test/java/edu/uc/EjercicioApiRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApiRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
